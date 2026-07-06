ecb82a8e2182c382176d5213f714e225edd4317c
marlosFranklin <marlos-gama@hotmail.com>
2026-07-06 15:08:21 -0300

    feat:add config redux-saga

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./root-reducer";
+import createSagaMiddleware from "redux-saga";
+import rootSaga from "./sagas";
+
+const sagaMiddleware = createSagaMiddleware();
 
 export const store = configureStore({
   reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(sagaMiddleware),
 });
+
+sagaMiddleware.run(rootSaga);
